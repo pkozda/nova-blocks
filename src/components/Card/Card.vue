91a0bc0a2ceb9b105be2685deedde96ee75b4dec
pkozda <pkozda@gmail.com>
2026-03-17 10:10:39 +0100

    [NB-6] added Card component with SCSS, test, and story (#6)

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nb-card">
+        <div v-if="$slots.header" class="nb-card__header">
+            <slot name="header" />
+        </div>
+  
+        <div class="nb-card__body">
+            <slot />
+        </div>
+  
+        <div v-if="$slots.footer" class="nb-card__footer">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue'
+  
+export default defineComponent({
+    name: 'Card'
+})
+</script>
+  
+  
+<style lang="scss" src="./Card.scss"></style>
